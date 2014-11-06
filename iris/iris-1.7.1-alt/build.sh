269f2e99322317f23be6384706666f2c0e3b65e8
benbovy <bbovy@ulg.ac.be>
2014-11-06 17:47:14 +0100

    fixed iris-1.7.1 and added iris-1.7.2

--- iris/iris-1.7.1-alt/build.sh
+++ iris/iris-1.7.1-alt/build.sh
@@ -11,4 +11,4 @@ SITECFG=lib/iris/etc/site.cfg
 echo "[System]" > $SITECFG
 echo "udunits2_path = $PREFIX/lib/libudunits2.${EXT}" >> $SITECFG
 
-python setup.py install
+$PYTHON setup.py --with-unpack build_ext --inplace --include-dirs=${PREFIX}/include --library-dirs=${PREFIX}/lib --rpath=${PREFIX}/lib install --prefix=$PREFIX
